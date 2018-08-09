2bda1ed83018f5043ca3f3ebee7f2d3607b6dafd
yairmalka06 <34348548+yairmalka06@users.noreply.github.com>
2018-08-09 07:56:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,6 @@ let xp = require("./xp.json");
 
 fs.readdir("./Commands/", (err, files) => {
 
-if(err) console.log(err);
-let jsfile = files.filter(f => f.split(".").pop() === "js");
-if(jsfile.length <= 0){
-  console.log("Couldn't find commands.");
-  return;
-}
 
 jsfile.forEach((f, i) =>{
   let props = require(`./commands/${f}`);
